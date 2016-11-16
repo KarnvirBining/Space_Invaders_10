ef115f74ccbbcaef4ea5c595fea977008d528b37
Rehan Theiveehathasan <t12.rehan@gmail.com>
2016-11-15 20:48:48 -0500

    Added bullet class initial code, and HUD class.

--- src/Bullet.java
+++ src/Bullet.java
@@ -1,4 +1,24 @@
+import java.awt.*;
 
-public class Bullet {
+public class Bullet extends GameObject {
 
+	public Bullet(float x, float y, ID id) {
+		super(x, y, id);
+		velX = 20;
+	}
+
+	@Override
+	public void tick() {
+
+	}
+
+	@Override
+	public void render(Graphics g) {
+
+	}
+
+	@Override
+	public Rectangle getBounds() {
+		return null;
+	}
 }
--- src/ID.java
+++ src/ID.java
@@ -2,5 +2,6 @@
 
 public enum ID {
 	Player(),
-	Alien();
+	Alien(),
+	Bullet();
 }
