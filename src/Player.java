f7f42ce3ba4f2036c0652a7434636e6f2eadc997
Puru Jetly <jetlyp@mcmaster.ca>
2016-11-15 23:07:38 -0500

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -6,8 +6,11 @@ import java.awt.Rectangle;
  */
 public class Player extends GameObject {
 
-	public Player(float x, float y, ID id) {
+    Handler handler;
+
+	public Player(float x, float y, ID id, Handler handler) {
 		super(x, y, id);
+		this.handler = handler;
 		
 
 		
@@ -18,10 +21,30 @@ public class Player extends GameObject {
 		x += velX;
 		y +=velY;
 		x = Game.Clamp((int)x, 3, Game.WIDTH -39);
+		collision();
 	}
+	
+	private void collision(){
+		for(int i= 0; i<handler.object.size(); i++){
+			GameObject tempObject = handler.object.get(i);
+			
+			if(tempObject.getId() == ID.Alien){//tempObject is BasicEnemy
+				//collision code
+				if(getBounds().intersects(tempObject.getBounds())){
+					//collision code
+					HUD.HEALTH -=50;
+					}
+				}
+
+		}
+	}
+
 
 	@Override
 	public void render(Graphics g) {
+//		Graphics2D g2d =(Graphics2D) g;
+//		g.setColor(Color.green);
+//		g2d.draw(getBounds());
 		g.setColor(Color.white);
 		g.fillRect((int)x, (int)y, 32, 32);
 		
@@ -29,8 +52,8 @@ public class Player extends GameObject {
 
 	@Override
 	public Rectangle getBounds() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return new Rectangle(0, Game.HEIGHT-75 ,Game.WIDTH, 32);
 	}
 
 
