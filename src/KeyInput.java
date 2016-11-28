f60dbf8e9c76b564ffd65cdf5a3ff9544bcb3998
Rehan Theiveehathasan <t12.rehan@gmail.com>
2016-11-28 18:14:08 -0500

    KeyInput.java updated allow pong paddle to move

--- src/KeyInput.java
+++ src/KeyInput.java
@@ -47,6 +47,7 @@ public class KeyInput extends KeyAdapter{
 	private Handler handler;
 	private boolean[] keyDown = new boolean[5];
 	private static float dx;
+	private static float dy;
 	private long lastShot = 0;
 
 	private void try2Shoot() {
@@ -60,7 +61,7 @@ public class KeyInput extends KeyAdapter{
 		}
 	}
 	
-	public KeyInput(Handler handler){
+	public KeyInput(Handler handler) {
 		this.handler = handler;
 		keyDown[0] = false;
 		keyDown[1] = false;
@@ -69,44 +70,54 @@ public class KeyInput extends KeyAdapter{
 		keyDown[4] = false;
 	}
 	
-	public void keyPressed(KeyEvent e){
+	public void keyPressed(KeyEvent e) {
 		int key =e.getKeyCode();
 		if (key == KeyEvent.VK_ESCAPE) System.exit(1);
 
-		for (int i = 0; i<handler.object.size(); i++){
+		for (int i = 0; i<handler.object.size(); i++) {
 			GameObject tempObject = handler.object.get(i);
 			dx = tempObject.x;
-			if (tempObject.getId()==ID.Player){
-				// key events for player 1
+
+			if (tempObject.getId()==ID.Player) {
+				// Key events for the ship
 				if(key == KeyEvent.VK_SPACE) {try2Shoot(); keyDown[0] = true;}
-				if(key == KeyEvent.VK_A) {tempObject.setVelX(-5);	dx-=5;  keyDown[1] = true;}
-				if(key == KeyEvent.VK_D) {tempObject.setVelX(5); dx+=5;	keyDown[2] = true;}
+				if(key == KeyEvent.VK_A) {tempObject.setVelX(-5); dx -= 5; keyDown[1] = true;}
+				if(key == KeyEvent.VK_D) {tempObject.setVelX(5);  dx += 5; keyDown[2] = true;}
+			}
+
+			if (tempObject.getId() == ID.Pong) {
+				// Key events for Pong paddle
+				if(key == KeyEvent.VK_W) {tempObject.setVelY(5);  dy += 5; keyDown[3] = true;}
+				if(key == KeyEvent.VK_S) {tempObject.setVelY(-5); dy -= 5; keyDown[4] = true;}
 			}
 		}
 	}
 	
-	public void keyReleased(KeyEvent e){
+	public void keyReleased(KeyEvent e) {
 		int key = e.getKeyCode();
 		
-		for (int i = 0; i<handler.object.size(); i++){
+		for (int i = 0; i<handler.object.size(); i++) {
 			GameObject tempObject = handler.object.get(i);
 
-			if (tempObject.getId()==ID.Player){
-				// key events for player 1
+			if (tempObject.getId() == ID.Player) {
+				// Key events for the ship
 				if(key == KeyEvent.VK_SPACE) keyDown[0] = false;//tempObject.setVelY(0);
 				if(key == KeyEvent.VK_A) keyDown[1] = false;//tempObject.setVelX(0);
 				if(key == KeyEvent.VK_D) keyDown[2] = false;//tempObject.setVelY(0);
 
-				//horizontal movement
-				if (!keyDown[1]&& !keyDown[2]) tempObject.setVelX(0);
-				
-				
+				// When nothing is pressed default velocity of x is 0
+				if (!keyDown[1] && !keyDown[2]) tempObject.setVelX(0);
 			}
-			
+
+			if (tempObject.getId() == ID.Pong) {
+				// Key events for Pong paddle
+				if(key == KeyEvent.VK_W) keyDown[3] = false;
+				if(key == KeyEvent.VK_S) keyDown[4] = false;
+
+				// When nothing is pressed default velocity of y is 0
+				if (!keyDown[3] && !keyDown[4]) tempObject.setVelY(0);
+			}
 		}
-		
 	}
-	
-	
 }
 
